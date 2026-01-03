4eeeb143874f50053722b813ccab4b7db0962a18
DaviMoreira27 <davimsantana2706@gmail.com>
2026-01-03 15:26:20 -0300

    feat: implemented remote state on s3

---
+++ infra/backend-dev.hcl
@@ -0,0 +1,6 @@
+bucket         = "terraform-state-893694643697"
+key            = "network/development.tfstate"
+region         = "us-east-1"
+dynamodb_table = "terraform-lock"
+encrypt        = true
+profile        = "default"
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,10 +1,24 @@
+/*
+That link is a tutorial, not necessarily a prescription for the best way to do something in all scenarios. In this scenario, if you are storing sensitive values in a .tfvars file (which I would not generally recommend), then yes, you absolutely want to add a line in .gitignore for *.tfvars. Storing sensitive information in .tfvars is not a good practice but committing sensitive information to Git should be one of the deadly sins.
+
+When dealing with sensitive information I usually create passwords via Secrets Manager (AWS) and then use reference attributes to retrieve them or I'll store them in parameter store and import them using using the SSM Parameter data source.
+
+data "aws_ssm_parameter" "my_sensitive_val" {
+  name = "/some/path"
+}
+
+Then you can reference it:
+
+data.aws_ssm_parameter.my_sensitive_val.value
+*/
+
 variable "environment" {
   description = "Environment that the resource will be created upon"
   type        = string
   default     = "development"
 
   validation {
-    condition     = contains(["development", "homolog", "staging", "production"], var.Environment)
+    condition     = contains(["development", "homolog", "staging", "production"], var.environment)
     error_message = "Environment must be one of: development, homolog, staging, production."
   }
 }
